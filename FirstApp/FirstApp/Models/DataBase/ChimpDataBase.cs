3b8dae821ec6c581663c59f91b0408e355e2f27f
Dmitry Podelnik <45734486+DmitryPodelnik@users.noreply.github.com>
2021-10-01 16:49:25 +0300

    v 0.0.5

---
+++ FirstApp/FirstApp/Models/DataBase/ChimpDataBase.cs
@@ -0,0 +1,22 @@
+﻿using FirstApp.Models.DataBase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace First_App.Models.DataBase
+{
+    public class ChimpDataBase
+    {
+        private ChimpDbContext _context;
+        public ChimpDbContext Context
+        {
+            get => _context;
+            set
+            {
+                _context = value;
+            }
+        }
+    }
+}
--- FirstApp/FirstApp/ViewModels/ChimpViewModel.cs
+++ FirstApp/FirstApp/ViewModels/ChimpViewModel.cs
@@ -1,22 +1,32 @@
-﻿using FirstApp.Models.DataBase;
+﻿using First_App.Models.DataBase;
+using FirstApp.Models.DataBase;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace First_App.ViewModels
 {
     public class ChimpViewModel : INotifyPropertyChanged
     {
-        private ChimpDbContext _database;
+        private ChimpDataBase _database = new();
+        private Chimp _chimpWindow = (Chimp)Application.Current.MainWindow;
 
         public ChimpViewModel()
         {
 
         }
 
+
+
         public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
     }
 }
