0bd899044fc6d222b1d636869482d4180cbcff33
Dmitry Podelnik <45734486+DmitryPodelnik@users.noreply.github.com>
2021-10-01 21:03:20 +0300

    v 0.2.2

--- FirstApp/FirstApp/Models/RegistryData/SavingRegistryData.cs
+++ FirstApp/FirstApp/Models/RegistryData/SavingRegistryData.cs
@@ -11,18 +11,16 @@ namespace First_App.Models.RegistryData
     {
         public void SaveUserData (string login, string password)
         {
-            RegistryKey currentUserKey = Registry.CurrentUser;
-            RegistryKey authKey = currentUserKey.CreateSubKey("ChimpAuthData");
+            using RegistryKey currentUserKey = Registry.CurrentUser;
+            using RegistryKey authKey = currentUserKey.CreateSubKey("ChimpAuthData");
             authKey.SetValue("login", login);
             authKey.SetValue("password", password);
-            authKey.Close();
         }
 
         public bool IsExistsKey(string key)
         {
-            RegistryKey currentUserKey = Registry.CurrentUser;
-            RegistryKey authKey = currentUserKey.OpenSubKey(key);
-            authKey.Close();
+            using RegistryKey currentUserKey = Registry.CurrentUser;
+            using RegistryKey authKey = currentUserKey.OpenSubKey(key);
 
             if (authKey != null)
             {
