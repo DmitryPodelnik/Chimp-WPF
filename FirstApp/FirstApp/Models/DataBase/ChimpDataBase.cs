fc5e6a4ab342cafb4570703ab15ae329cd562cba
Dmitry Podelnik <45734486+DmitryPodelnik@users.noreply.github.com>
2021-10-01 19:19:29 +0300

    v 0.1.3

--- FirstApp/FirstApp/Models/DataBase/ChimpDataBase.cs
+++ FirstApp/FirstApp/Models/DataBase/ChimpDataBase.cs
@@ -1,9 +1,12 @@
 ﻿using FirstApp.Models.DataBase;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
 
 namespace First_App.Models.DataBase
 {
@@ -17,6 +20,32 @@ namespace First_App.Models.DataBase
             {
                 _context = value;
             }
+        }
+
+        public string ConnectionString { get; set; }
+        public DbContextOptions<ChimpDbContext> Options { get; set; }
+
+        public ChimpDataBase()
+        {
+            var configuration =
+                 new ConfigurationBuilder()
+                     .SetBasePath(Directory.GetCurrentDirectory())
+                     .AddJsonFile("appsettings.json")
+                     .Build();
+
+
+            // Получаем строку подключения из файла appsettings.json
+            ConnectionString = configuration.GetConnectionString("DefaultConnection");
+
+            // Создаем объект контекста EF, указываем ему строку соединения и
+            // получаем объект настроек для конструктора объекта контекста EF
+            var options =
+                new DbContextOptionsBuilder<ChimpDbContext>()
+                    .UseSqlServer(ConnectionString)
+                    .Options;
+            Options = options;
+
+            _context = new ChimpDbContext(options);
         }
     }
 }
