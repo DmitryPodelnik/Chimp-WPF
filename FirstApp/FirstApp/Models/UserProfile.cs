1789458af767fe56f4c18cd974da81f836baa5ac
Dmitry Podelnik <45734486+DmitryPodelnik@users.noreply.github.com>
2021-10-01 17:05:06 +0300

    v 0.0.6

---
+++ FirstApp/FirstApp/Models/UserProfile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace First_App.Models
+{
+    [Table("UserProfiles")]
+    public class UserProfile
+    {
+        [Column("Id")]  // Можно было не указывать потому, что так было бы по умолчанию, благодаря соглашению о наименованиях EF
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+    }
+}
