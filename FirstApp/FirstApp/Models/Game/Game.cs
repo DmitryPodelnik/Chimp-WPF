ab0b5f90296294cec2ffd4fe5d917cf4ca3ac0e0
Dmitry Podelnik <45734486+DmitryPodelnik@users.noreply.github.com>
2021-10-05 20:28:44 +0300

    v 0.6.0

--- FirstApp/FirstApp/Models/Game/Game.cs
+++ FirstApp/FirstApp/Models/Game/Game.cs
@@ -1,4 +1,5 @@
 ﻿using First_App.Interfaces;
+using First_App.Models.Commands;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,6 +7,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace First_App.Models.Game
 {
@@ -33,7 +35,20 @@ namespace First_App.Models.Game
         {
             InitializeGameCubes();
         }
-        
+
+        private RelayCommand _deleteButtonCommand;
+        public RelayCommand DeleteButtonCommand
+        {
+            get
+            {
+                return _deleteButtonCommand =
+                (_deleteButtonCommand = new RelayCommand(obj =>
+                {
+                    MessageBox.Show("Hello");
+                }));
+            }
+        }
+
         private void InitializeGameCubes()
         {
             _numberGenerator.GenerateNumbersForCubes(_cubes);
@@ -47,10 +62,28 @@ namespace First_App.Models.Game
                 Button newButton = new();
                 newButton.Content = _cubes[i].Value;
                 newButton.Name = $"playButton{i}";
+
+                //// создаем привязку команды
+                //CommandBinding commandBinding = new();
+                //// устанавливаем команду
+                //commandBinding.Command = DeleteButtonCommand;
+                //// устанавливаем метод, который будет выполняться при вызове команды
+                //commandBinding.Executed += DeleteButton_Executed;
+                //// добавляем привязку к коллекции привязок элемента Button
+                //newButton.CommandBindings.Add(commandBinding);
+
+                newButton.Click += DeleteButton_Executed;
+
                 Grid.SetRow(newButton, Convert.ToInt32(_cubes[i].Coords.Y));
                 Grid.SetColumn(newButton, Convert.ToInt32(_cubes[i].Coords.X));
                 _chimpWindow.playGrid.Children.Add(newButton);
             }
+        }
+
+        private void DeleteButton_Executed(object sender, RoutedEventArgs e)
+        {
+            Button button = (Button)e.Source;
+            _chimpWindow.playGrid.Children.Remove(button);
         }
 
         public void Notify(object sender, string ev)
