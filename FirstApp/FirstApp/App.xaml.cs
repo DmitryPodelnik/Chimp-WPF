31347ac1cba46459e50f6294eb93a27a125f77bb
Dmitry Podelnik <45734486+DmitryPodelnik@users.noreply.github.com>
2021-10-06 20:14:27 +0300

    v 0.6.9

--- FirstApp/FirstApp/App.xaml.cs
+++ FirstApp/FirstApp/App.xaml.cs
@@ -13,5 +13,6 @@ namespace FirstApp
     /// </summary>
     public partial class App : Application
     {
+
     }
 }
---
+++ FirstApp/FirstApp/Navigation/Navigator.cs
@@ -0,0 +1,47 @@
+﻿using First_App.Models.Commands;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace First_App.Navigation
+{
+    class Navigator : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+
+        private object _currentViewModel;
+        public object CurrentViewModel
+        {
+            get => _currentViewModel;
+            set
+            {
+                _currentViewModel = value;
+                OnPropertyChanged("CurrentViewModel");
+            }
+        }
+        public RelayCommand UpdateCurrentViewModelCommand { get; }
+
+        private static Navigator _navigator;
+        public static Navigator Create()
+        {
+            if (_navigator == null)
+            {
+                _navigator = new();
+            }
+            return _navigator;
+        }
+
+        private Navigator()
+        {
+            UpdateCurrentViewModelCommand = new RelayCommand(o => CurrentViewModel = o);
+        }
+    }
+}
