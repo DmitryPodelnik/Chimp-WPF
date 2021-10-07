0be4646eeac114733edd02307b14c57c8af257b8
Dmitry Podelnik <45734486+DmitryPodelnik@users.noreply.github.com>
2021-10-07 20:02:57 +0300

    0.7.9

--- FirstApp/FirstApp/Services/Authentication/Authenticator.cs
+++ FirstApp/FirstApp/Services/Authentication/Authenticator.cs
@@ -4,14 +4,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace First_App.Services.Authentication
 {
     /// <summary>
     ///     Class of authentication to user.
     /// </summary>
-    class Authenticator
+    public class Authenticator
     {
+        private Chimp _chimpWindow = (Chimp)Application.Current.MainWindow;
+
         private static Authenticator _instance = null;
         public static Authenticator Create()
         {
@@ -40,5 +43,46 @@ namespace First_App.Services.Authentication
             CurrentUser = SavingRegistryData.GetCurrentUser();
         }
         public bool IsLoggedIn => CurrentUser != null;
+
+        /// <summary>
+        ///     Verify whether the data of user are correct.
+        /// </summary>
+        public void CheckAuthorization()
+        {
+            // if login or password box is null or empty then error message
+            if (String.IsNullOrEmpty(_chimpWindow.loginTextBox.Text) ||
+                String.IsNullOrEmpty(_chimpWindow.passwordBox.Password))
+            {
+                MessageBox.Show("Incorrect login or password!", "Error", MessageBoxButton.OK);
+
+                return;
+            }
+
+            // verify whether login and password are correct and exists and compares in the database
+            bool result = _database.IsAuthorized(
+                _chimpWindow.loginTextBox.Text,
+                _chimpWindow.passwordBox.Password
+                );
+
+            // if correct then save user data in the registry
+            if (result)
+            {
+                SavingRegistryData registry = new();
+                registry.SaveUserData(
+                    _chimpWindow.loginTextBox.Text,
+                    _chimpWindow.passwordBox.Password
+                    );
+
+                // show success message box and welcome message and hide authorization panel
+                LoginSuccessActions();
+                EnableLeftButtonBeforeAuth();
+            }
+            else // or show error message
+            {
+                MessageBox.Show("Incorrect login or password!", "Error", MessageBoxButton.OK);
+            }
+            // clear password box field
+            _chimpWindow.passwordBox.Password = "";
+        }
     }
 }
--- FirstApp/FirstApp/ViewModels/AuthorizationViewModel.cs
+++ FirstApp/FirstApp/ViewModels/AuthorizationViewModel.cs
@@ -1,10 +1,14 @@
-﻿using System;
+﻿using First_App.Models.Commands;
+using First_App.Models.DataBase;
+using First_App.Services.Authentication;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace First_App.ViewModels
 {
@@ -17,6 +21,51 @@ namespace First_App.ViewModels
         public void OnPropertyChanged([CallerMemberName] string prop = "")
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+
+        // field to work with database
+        private ChimpDataBase _database = new();
+        // field of main window
+        private Chimp _chimpWindow = (Chimp)Application.Current.MainWindow;
+
+        private Authenticator _authenticator = new();
+
+        /// <summary>
+        ///     Command after clicking login button.
+        /// </summary>
+        private RelayCommand _loginCommand;
+        public RelayCommand LoginCommand
+        {
+            get
+            {
+                return _loginCommand =
+                (_loginCommand = new RelayCommand(obj =>
+                {
+                    // verify whether the data of user are correct
+                    _authenticator.CheckAuthorization();
+                }));
+            }
+        }
+
+        /// <summary>
+        ///     Show success message box and welcome message and hide authorization panel.
+        /// </summary>
+        private void LoginSuccessActions()
+        {
+            MessageBox.Show("You have been successfully logged in!", "Authorization", MessageBoxButton.OK);
+            _chimpWindow.accountNameTextBlock.Text = $"Hello, {_chimpWindow.loginTextBox.Text}!";
+            _chimpWindow.authorizationPanel.Visibility = Visibility.Hidden;
+        }
+
+        /// <summary>
+        ///     Enable left buttons.
+        /// </summary>
+        private void EnableLeftButtonBeforeAuth()
+        {
+            //_chimpWindow.recordsButton.IsEnabled = true;
+            //_chimpWindow.profileButton.IsEnabled = true;
+            //_chimpWindow.playButton.IsEnabled = true;
+            //_chimpWindow.mainTabButton.IsEnabled = true;
         }
     }
 }
